72978ed77b6cc40467a73d471c585d43be2440f4
Lenschow19 <55918642+Lenschow19@users.noreply.github.com>
2021-02-26 12:11:37 +0100

    Material installed

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [ChatComponent],
-  imports: [
-    CommonModule,
-    ChatRoutingModule,
-    ReactiveFormsModule
-  ]
+    imports: [
+        CommonModule,
+        ChatRoutingModule,
+        ReactiveFormsModule,
+        MatButtonModule
+    ]
 })
 export class ChatModule { }
